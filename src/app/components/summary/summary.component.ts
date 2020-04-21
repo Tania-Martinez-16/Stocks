fa5895946fa3acb15d3ff4874bf1ad98ea83b434
Tania Georgina Martinez Tuz <16470257@itcampeche.edu.mx>
2020-04-21 10:51:05 -0500

    Se agregó el componente Dashboard

--- src/app/components/summary/summary.component.ts
+++ src/app/components/summary/summary.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-summary',
+  selector: 'summary',
   templateUrl: './summary.component.html',
   styleUrls: ['./summary.component.css']
 })
-export class SummaryComponent implements OnInit {
 
-  constructor() { }
+export class SummaryComponent {
+  @Input() stock: any;
 
-  ngOnInit(): void {
+  isNegative() {
+    return (this.stock && this.stock.change < 0);
   }
 
+  isPositive() {
+    return (this.stock && this.stock.change > 0);
+  }
 }
